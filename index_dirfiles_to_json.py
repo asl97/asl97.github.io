ed11ba02ba8a5ddb4988a27b1109fb362cee2385
asl97 <asl97@airmail.cc>
2024-01-21 05:27:37 +0800

    Remove path starting with . from global json

--- index_dirfiles_to_json.py
+++ index_dirfiles_to_json.py
@@ -2,8 +2,13 @@ import os
 import json
 
 def run():
+    basic_filters = ['.']
     jsons = {}
     for root, dirs, files in os.walk('.'):
+        for dir in dirs.copy():
+            for s in basic_filters:
+                if dir.startswith(s):
+                    dirs.remove(dir)
         with open(os.path.join(root, "index.json"),'w') as f:
             json.dump(files, f)
         jsons[root] = files
