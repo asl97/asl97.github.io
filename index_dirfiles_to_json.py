ca2618347ff70ecebe9bcfd02653a9ffd2a6783d
asl97 <asl97@airmail.cc>
2024-05-03 04:36:29 +0800

    Attempt to include json_index.json in root index.json

--- index_dirfiles_to_json.py
+++ index_dirfiles_to_json.py
@@ -4,16 +4,17 @@ import json
 def run():
     basic_filters = ['.']
     jsons = {}
-    for root, dirs, files in os.walk('.'):
-        for dir in dirs.copy():
-            for s in basic_filters:
-                if dir.startswith(s):
-                    dirs.remove(dir)
-        with open(os.path.join(root, "index.json"),'w') as f:
-            json.dump([dirs, files], f)
-        jsons[root] = files
-    with open('json_index.json', 'w') as f:
-        json.dump(jsons, f)
+    with open('json_index.json', 'w') as json_index:
+        for root, dirs, files in os.walk('.'):
+            for dir in dirs.copy():
+                for s in basic_filters:
+                    if dir.startswith(s):
+                        dirs.remove(dir)
+            with open(os.path.join(root, "index.json"),'w') as f:
+                json.dump([dirs, files], f)
+            jsons[root] = files
+
+        json.dump(jsons, json_index)
 
 if __name__ == "__main__":
     run()
